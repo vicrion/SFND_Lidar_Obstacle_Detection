d91aba68864688b0406ce13b5044f3657b714880
VR <vicrion@pm.me>
2024-11-05 19:38:18 -0500

    updated segemntation function with customized ransac 3d

--- src/processPointClouds.cpp
+++ src/processPointClouds.cpp
@@ -2,7 +2,11 @@
 
 #include "processPointClouds.h"
 
+#include <unordered_set>
+#include <functional>
+
 #include <spdlog/spdlog.h>
+#include <Eigen/Dense>
 
 
 //constructor:
@@ -67,25 +71,28 @@ std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT
 {
     // Time segmentation process
     auto startTime = std::chrono::steady_clock::now();
-	pcl::PointIndices::Ptr inliers(new pcl::PointIndices ());
-    // find inliers for the cloud.
-    pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients());
-    // Create the segmentation object
-    pcl::SACSegmentation<PointT> seg;
-    // Optional
-    seg.setOptimizeCoefficients(true);
-    // Mandatory
-    seg.setModelType(pcl::SACMODEL_PLANE);
-    seg.setMethodType(pcl::SAC_RANSAC);
-    seg.setMaxIterations(maxIterations);
-    seg.setDistanceThreshold(distanceThreshold);
 
-    seg.setInputCloud (cloud);
-    seg.segment (*inliers, *coefficients);
-    if (inliers->indices.size () == 0)
-    {
-        spdlog::error("Could not estimate a planar model for the given dataset.");
-    }
+    auto inliers = ransac3d(cloud, maxIterations, distanceThreshold);
+
+	// pcl::PointIndices::Ptr inliers(new pcl::PointIndices ());  
+    // // find inliers for the cloud.
+    // pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients());
+    // // Create the segmentation object
+    // pcl::SACSegmentation<PointT> seg;
+    // // Optional
+    // seg.setOptimizeCoefficients(true);
+    // // Mandatory
+    // seg.setModelType(pcl::SACMODEL_PLANE);
+    // seg.setMethodType(pcl::SAC_RANSAC);
+    // seg.setMaxIterations(maxIterations);
+    // seg.setDistanceThreshold(distanceThreshold);
+
+    // seg.setInputCloud (cloud);
+    // seg.segment (*inliers, *coefficients);
+    // if (inliers->indices.size () == 0)
+    // {
+    //     spdlog::error("Could not estimate a planar model for the given dataset.");
+    // }
     
     auto endTime = std::chrono::steady_clock::now();
     auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
@@ -168,4 +175,111 @@ std::vector<boost::filesystem::path> ProcessPointClouds<PointT>::streamPcd(std::
 
     return paths;
 
+}
+
+template <typename PointT>
+typename pcl::PointIndices::Ptr ProcessPointClouds<PointT>::ransac3d(typename pcl::PointCloud<PointT>::Ptr cloud, int maxIterations, float distanceThreshold)
+{
+	srand(time(NULL));
+
+	const int SSET_SIZE = 3; // min 3 points to fit the plane in
+	if (cloud->size()<= SSET_SIZE){
+		spdlog::error("The cloud size={} is too small for RANSAC3.", cloud->size() );
+		return {};
+	}
+
+	auto getRandomSet = [&]() 
+	{
+		std::unordered_set<int> subset;
+
+		while(subset.size() < SSET_SIZE){
+			int i = rand() % cloud->size();
+			subset.insert(i);
+		}
+
+		auto it = subset.begin();
+		int first = *it;
+		++it;
+		int second = *it;
+		++it;
+		int third = *it;
+
+		auto p1 = cloud->at(first);
+		auto p2 = cloud->at(second);	
+		auto p3 = cloud->at(third);			
+
+		return std::make_tuple(p1, p2, p3);
+	};
+
+	auto getPlaneEq = [&](const pcl::PointXYZ &point1, const pcl::PointXYZ &point2, const pcl::PointXYZ &point3)
+	{
+		Eigen::Vector3f p1(point1.x, point1.y, point1.z);
+		Eigen::Vector3f p2(point2.x, point2.y, point2.z);
+		Eigen::Vector3f p3(point3.x, point3.y, point3.z);
+
+		Eigen::Vector3f v1 = p2 - p1;
+		Eigen::Vector3f v2 = p3 - p1;
+
+		Eigen::Vector3f normal = v1.cross(v2);
+		normal.normalize();
+
+		// Plane equation: Ax + By + Cz + D = 0
+		float A = normal.x();
+		float B = normal.y();
+		float C = normal.z();
+		float D = -(A * p1.x() + B * p1.y() + C * p1.z());
+
+		return pcl::PointXYZI(A,B,C,D);
+	};
+
+	auto getDistance = [](const pcl::PointXYZI& plane, const pcl::PointXYZ& point)
+	{
+		float div = std::sqrt(plane.x*plane.x + plane.y*plane.y + plane.z*plane.z);
+
+		if (std::fabs(div) < 1e-3){
+			return std::numeric_limits<float>::max();
+		}
+
+		float d = std::fabs(plane.x * point.x + plane.y * point.y + plane.z * point.z + plane.intensity) / div;
+		
+		return d;
+	};
+	
+	auto getNumInliers = [&](const pcl::PointXYZI& plane)
+	{
+		int numIn = 0;
+		for (unsigned int i=0; i<cloud->size(); ++i){
+			if ( getDistance(plane, cloud->at(i)) <= distanceThreshold ){
+				numIn++;
+			}
+		}
+		return numIn;
+	};
+
+	int numMaxIn = 0;
+	pcl::PointXYZI bestPlane;
+	while(maxIterations--){
+		// randomly sample subset:
+		auto [p1, p2, p3] = getRandomSet();
+		
+		// fit plane:
+		auto plane = getPlaneEq(p1, p2, p3);
+
+		auto numIn = getNumInliers(plane);
+		numMaxIn = std::max(numIn, numMaxIn);
+
+		if (numMaxIn == numIn){
+			bestPlane = plane;
+		}
+	}
+
+    pcl::PointIndices::Ptr inliers(new pcl::PointIndices ());
+	// Return indicies of inliers from fitted line with most inliers
+	for (unsigned int i=0; i<cloud->size(); ++i){
+		if (getDistance(bestPlane, cloud->at(i)) <= distanceThreshold ){
+            inliers->indices.push_back(int(i));
+		}
+	}
+
+    return inliers;
 }
